df2d8fed0bd11d50bacbae067e3c61ca78696ecc
Carlos Garcia Castellanos <chars24@gmail.com>
2021-08-30 23:39:19 -0500

    Logic working fine, missing rules to handle css

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const boardScore = document.querySelector('#boardscore')
+const boardScoreResult = document.querySelector('#boardScoreResult')
+const userChoose = document.querySelector('#userChoose')
+const pcChoose = document.querySelector('#pcChoose')
+const buttonReset = document.querySelector('#reset')
+const playAgain = document.querySelector('#play')
+
 // An object of the colors with their game options
 const optionsValues = {
     Blue: 'Paper',
@@ -24,14 +31,27 @@ for (values in optionsValues) {
 
 // Get a random option for PC
 let randomOptionPC = () => {
-    return Object.values(optionsValues)[Math.floor(Math.random() * Object.values(optionsValues).length)]
+    return Object.values(optionsValues)[
+        Math.floor(Math.random() * Object.values(optionsValues).length)
+    ]
+}
+
+// Function to draw the images
+const drawImage = (imageOptions) => {
+    let usrImg = document.createElement('img')
+    usrImg.src = `./assets/${imageOptions}.png`
+    return usrImg
 }
 
 // Play the game function
 let playGame = (optionUser) => {
     let optionPC = randomOptionPC()
+    
+    boardScoreResult.style.display = 'block'
 
-    console.log(optionUser, optionPC)
+    // Draw the images
+    userChoose.appendChild(drawImage(optionUser))
+    pcChoose.appendChild(drawImage(optionPC))
     
     // logic of the game
     if (optionUser === 'Rock' && optionPC === 'Scissors') {
@@ -47,3 +67,12 @@ let playGame = (optionUser) => {
         console.log('Perdiste')
     }
 }
+
+// Buttons reset and play
+buttonReset.addEventListener('click', () => {
+    console.log('reset')
+})
+
+playAgain.addEventListener('click', () => {
+    boardScoreResult.style.display = 'none'
+})

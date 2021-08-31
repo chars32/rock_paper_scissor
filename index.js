5527d81d6608b80d229c91e4535b813486e71a1f
Carlos Garcia Castellanos <chars24@gmail.com>
2021-08-30 19:35:07 -0500

    BoardGame working 90%

--- index.js
+++ index.js
@@ -1,29 +1,49 @@
-// Listing all posible options values
+// An object of the colors with their game options
 const optionsValues = {
     Blue: 'Paper',
     Red: 'Rock',
     Yellow: 'Scissors'
 }
 
-const getValue = (valor) => {
-    return valor
+/* 
+    Function to return the value of the element
+    with the addeventlistener
+
+*/
+const getValue = (item) => {
+    return item
 }
 
-for (valores in optionsValues) {
-    let valor = document.querySelector(`.${valores}`)
-    valor.addEventListener('click', () => {
-        playGame(getValue(valor.name))
+// A loop to add the eventlistener to the img elements
+for (values in optionsValues) {
+    let itemSelected = document.querySelector(`.${values}`)
+    itemSelected.addEventListener('click', () => {
+        playGame(getValue(itemSelected.name))
     })
 }
 
 // Get a random option for PC
 let randomOptionPC = () => {
-    return optionsValues[Math.floor(Math.random() * optionsValues.length)]
+    return Object.values(optionsValues)[Math.floor(Math.random() * Object.values(optionsValues).length)]
 }
 
 // Play the game function
 let playGame = (optionUser) => {
-    // let optionPC = randomOptionPC()
-    // console.log(optionUser, optionPC.name)
-    console.log(optionUser)
+    let optionPC = randomOptionPC()
+
+    console.log(optionUser, optionPC)
+    
+    // logic of the game
+    if (optionUser === 'Rock' && optionPC === 'Scissors') {
+        console.log('Ganaste')
+    } else if (optionUser === 'Scissors' && optionPC === 'Paper') {
+        console.log('Ganaste')
+    } else if (optionUser === 'Paper' && optionPC === 'Rock') {
+        console.log('Ganaste')
+    } else if (optionUser === optionPC) {
+        console.log('Empate')
+    }
+    else {
+        console.log('Perdiste')
+    }
 }
